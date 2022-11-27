c483429a1f5874e4d628cbbddff07189397860db
Khalid Obaide <sayedkhalidobaide@gmail.com>
2022-11-27 22:22:03 +0300

    Remove output option from webpack config

--- templates/application/react/base/webpack.config.js
+++ templates/application/react/base/webpack.config.js
@@ -3,9 +3,9 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 
 const deps = require("./package.json").dependencies;
 module.exports = {
-  output: {
-    publicPath: "http://localhost:{{PORT}}/",
-  },
+  // output: {
+  //   publicPath: "http://localhost:{{PORT}}/",
+  // },
 
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
